40790f8cb231164aa2d729639a058ac27e6bf3d4
Lin-Yuying <61513895+Lin-Yuying@users.noreply.github.com>
2026-07-07 11:47:54 +0800

    Update 01.genotyping.sh

--- 01.genotyping.sh
+++ 01.genotyping.sh
@@ -1,6 +1,6 @@
 bcftools=~/bin/bcftools-1.16/bcftools
 ref=~/3.SV/1.alignment_male/guppy_female.fa
-region_name=$1 #batch calling, I seperate it into 4 regions
+region_name=$1 #batch calling, I seperated it into 4 
 bam_file=$2 #please change when use
 ${bcftools} mpileup -Ou -q 20 -Q 20 -R region${region_name}.guppy.female.txt --skip-indels -a FORMAT/AD,FORMAT/DP -f $ref -b ${bam_file}.pop.txt | ${bcftools} call -mv -Oz -a GQ,GP -o ${bam_file}.region${region_name}.vcf.gz
 
